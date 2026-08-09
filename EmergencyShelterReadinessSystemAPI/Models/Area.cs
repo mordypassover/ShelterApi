8b270d524f7cef02c6796716312b9b733a7479e3
Mordy Pesach <mordypassover@gmail.com>
2026-08-09 11:06:18 +0300

    added Area model

---
+++ EmergencyShelterReadinessSystemAPI/Models/Area.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmergencyShelterReadinessSystemAPI.Models;
+
+public class Area
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string City { get; set; } = string.Empty;
+    [Required]
+    public string Neighborhood { get; set; } = string.Empty;
+
+    [Required]
+    public int AreaCode { get; set; }
+
+    [Required]
+    public int RiskLevel { get; set; }
+
+
+}
