e391f9e31ce2efd80bb95e167722ddd74015703a
Mordy Pesach <mordypassover@gmail.com>
2026-08-09 11:11:34 +0300

    added Shelter model

---
+++ EmergencyShelterReadinessSystemAPI/Models/Shelter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace EmergencyShelterReadinessSystemAPI.Models;
+
+public class Shelter
+{
+    public int Id {  get; set; }
+    public string Name {  get; set; }
+    public string Street {  get; set; }
+    public int BuildingNumber {  get; set; }
+
+
+}
