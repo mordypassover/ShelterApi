b0bec492e2857800766029dd075d8b75bc803f13
Mordy Pesach <mordypassover@gmail.com>
2026-08-09 14:50:19 +0300

    fixed databas structur, DefectsCount structur

--- EmergencyShelterReadinessSystemAPI/Migrations/20260809113815_fixDefectsCountTypo.Designer.cs
+++ EmergencyShelterReadinessSystemAPI/Migrations/20260809113815_fixDefectsCountTypo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EmergencyShelterReadinessSystemAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmergencyShelterReadinessSystemAPI.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260809113815_fixDefectsCountTypo")]
+    partial class fixDefectsCountTypo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmergencyShelterReadinessSystemAPI/Migrations/20260809113815_fixDefectsCountTypo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmergencyShelterReadinessSystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixDefectsCountTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DefectsCountv",
+                table: "Inspections",
+                newName: "DefectsCount");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DefectsCount",
+                table: "Inspections",
+                newName: "DefectsCountv");
+        }
+    }
+}
